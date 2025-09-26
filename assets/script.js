b66f78bc30c64f846d795afb0013e61940d6f63f
alfirda0us <kenzie.ahnaffirdaus@gmail.com>
2025-09-26 11:43:19 +0700

    mantap

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function hamburger() {
+    const navbar = document.querySelector('.dropdown')
+    navbar.style.transform = "translateX(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector('.dropdown')
+    navbar.style.transform = "translateX(-500px)"
+}

e2cb29071686d60004cce693ca7b63e80633d4dc
alfirda0us <kenzie.ahnaffirdaus@gmail.com>
2025-09-28 11:17:52 +0700

    responsive stuff

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-function hamburger() {
-    const navbar = document.querySelector('.dropdown')
-    navbar.style.transform = "translateX(0px)"
-}
-function cancel(){
-    const navbar = document.querySelector('.dropdown')
-    navbar.style.transform = "translateX(-500px)"
-}
